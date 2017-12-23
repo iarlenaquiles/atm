c0d58035bd85aaab0aeef2519d15fd02e9e618f6
Iarlen <iarlensilva775@gmail.com>
2017-12-23 17:20:38 -0300

    Criando projeto

---
+++ src/components/BarraNavegacao.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class BarraNavegacao extends Component {
+  render() {
+    return (
+     <Text>ATM Consultoria</Text>
+    );
+  }
+}
