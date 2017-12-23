56392cde360cd4d555baec8c0c9c7f6b73319ce8
Iarlen <iarlensilva775@gmail.com>
2017-12-23 17:26:57 -0300

    Criando a barra de titulo

--- src/components/BarraNavegacao.js
+++ src/components/BarraNavegacao.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import {
-  Text
+  Text,
+  StyleSheet,
+  View
 } from 'react-native';
 
 export default class BarraNavegacao extends Component {
   render() {
     return (
-     <Text>ATM Consultoria</Text>
+	<View style={styles.BarraTitulo}>
+      <Text style={styles.titulo}>ATM Consultoria</Text>
+    </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+	BarraTitulo: {
+		backgroundColor: '#CCC',
+		padding: 10,
+		height: 60
+	},
+	titulo: {
+		flex: 1,
+		fontSize: 18,
+		textAlign: 'center',
+		color: '#000' 
+	}
+});
