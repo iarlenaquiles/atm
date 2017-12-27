4fee2361221ada961dbe7388d0446a94e2ee2655
Iarlen <iarlensilva775@gmail.com>
2017-12-27 13:29:26 -0300

    Melhorando a transição da cena

--- src/components/CenaEmpresa.js
+++ src/components/CenaEmpresa.js
@@ -14,7 +14,7 @@ const imgEmpresa = require('../imgs/detalhe_empresa.png');
 export default class CenaEmpresa extends Component {
   render() {
     return (
-	<View>
+	<View style={{ flex: 1, backgroundColor: '#FFF' }}>
         <StatusBar backgroundColor='#EC7148' />
         <BarraNavegacao voltar navigator={this.props.navigator} />  
 
