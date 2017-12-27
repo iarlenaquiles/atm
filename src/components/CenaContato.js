c57b98a222e836f1717fd239c6ec19b4312bc7b3
Iarlen <iarlensilva775@gmail.com>
2017-12-26 22:37:30 -0300

    Criando o cabeçalho da cena

--- src/components/CenaContato.js
+++ src/components/CenaContato.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import {
   StatusBar,
-  View
+  View,
+  Image,
+  Text,
+  StyleSheet
 } from 'react-native';
 
 import BarraNavegacao from './BarraNavegacao';
@@ -13,8 +16,34 @@ export default class CenaContato extends Component {
     return (
 	<View>
         <StatusBar backgroundColor='#61BD8C' />
-        <BarraNavegacao voltar navigator={this.props.navigator} />         
+        <BarraNavegacao voltar navigator={this.props.navigator} />  
+
+        <View style={styles.cabecalho}>
+          <Image source={imgContato} />
+          <Text style={styles.txtTitulo}>Contatos</Text>
+        </View>       
      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cabecalho: {
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  txtTitulo: {
+    fontSize: 30,
+    color: '#61BD8C',
+    marginLeft: 10,
+    marginTop: 25
+  },
+  detalheCliente: {
+    padding: 20,
+    marginTop: 10
+  },
+  txtDetalheCliente: {
+    fontSize: 18,
+    marginLeft: 20
+  }
+});
